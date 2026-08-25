635daf63233fcb537d3e76230247ed572cb9d95b
Bhagyashree Patil <84381569+bhagyashree-ap@users.noreply.github.com>
2026-08-25 15:28:44 +0530

    Time: 0 ms (100%), Space: 19.4 MB (27.88%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Inorder: Left -> Root -> Right
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+        res=[]
+        stack=[]
+        curr=root
+
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr=curr.left
+
+            curr=stack.pop()  #get node
+            res.append(curr.val)
+
+            curr=curr.right
+        
+        return res
+        
